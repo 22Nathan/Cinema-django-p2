2eca26651456cfa12634713d375eda005b6e6d90
22Nathan <64403796+22Nathan@users.noreply.github.com>
2023-07-19 14:40:24 +0200

    :alien:

--- back/cinemaProject/cinemaApp/models.py
+++ back/cinemaProject/cinemaApp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    ID_user = models.IntegerField()
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    email = models.EmailField()
+    mdp = models.CharField(max_length=20)
+
+class Film(models.Model):
+    ID_film = models.IntegerField()
+    titre = models.CharField(max_length=100)
+    resume = models.CharField(max_length=500)
+    duree = models.IntegerField()
+    estSpecial = models.BooleanField()
+
+class Salle(models.Model):
+    ID_salle = models.IntegerField()
+    nom = models.CharField(max_length=20)
+    capacite = models.IntegerField()
+
+class Seance(models.Model):
+    ID_seance = models.IntegerField()
+    ID_film = models.ForeignKey('Film', on_delete=models.DO_NOTHING)
+    ID_salle = models.ForeignKey('Salle', on_delete=models.DO_NOTHING)
+    date = models.DateField()
+    heure = models.TimeField()
+    prix = models.FloatField()
